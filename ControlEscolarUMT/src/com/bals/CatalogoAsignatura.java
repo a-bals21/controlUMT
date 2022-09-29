d9a2cda70794d4855ce0d40fb63cab40398aef2f
kevincamelo5 <kcamelosuaste@gmail.com>
2022-09-29 14:33:32 -0500

    Update CatalogoAsignatura
    Agregué el menú cargaAcademica y el metodo clave

--- ControlEscolarUMT/src/com/bals/CatalogoAsignatura.java
+++ ControlEscolarUMT/src/com/bals/CatalogoAsignatura.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * catalogo que lee informacion relacionada con las asignaturas en el semestre.
+ * Esta clase lee informacion relacionada con las asignaturas en el semestre.
+ * @autor kevin leandro camelo suaste,
  * 
  */
 public class CatalogoAsignatura {
@@ -18,28 +19,38 @@ public class CatalogoAsignatura {
         return scanner.nextLine();
     }
 
+    public String SolicitarClave() {
+    	System.out.println("Escribe la clave de la materia");
+    	return scanner.nextLine();
+    }
+    
     public Integer SolicitarSemestre(){
         System.out.println("Escribe la asignatura a la que pertenece");
         return scanner.nextInt();
     }
-
+    
     public Integer SolicitarCreditos(){
         System.out.println("Escribe los creditos que vale la asignatura");
         return scanner.nextInt();
     }
+    
     public void ReadAsignaturas (Asignatura asignatura){
         System.out.println("\n\tAsignatura");
         System.out.println(asignatura);
     }
+    
     public void readAsignatura(){System.out.println("\n\tNo existe materia conocida\n");}
 
+    //metodo
     public void readTAsignatura(ArrayList<Asignatura> asignaturas){
         System.out.println("\n\tLista de asignaturas");
         asignaturas.forEach(System.out::print);
         System.out.println("\n");
     }
+    
+    //metodo
     public Integer menu(){
-        System.out.println("\n\tMENÚ MODIFICACIÓN\n");
+        System.out.println("\n\tMENÚ ASIGNATURA\n");
         System.out.println("1.- agregar asignatura");
         System.out.println("2.- borrar asignatura");
         System.out.println("3.- actualizar la asignatura");
@@ -52,33 +63,51 @@ public class CatalogoAsignatura {
         return opcion;
     }
 
-    public Integer menuModificarAsi(){
+    public Integer menuModificarAsignatura(){
         System.out.println("\n\tMenú Modificación\n");
         System.out.println("1.- Nombre de la asignatura");
-        System.out.println("2.- Semestre al que pertenece la asignatura");
-        System.out.println("3.- creditos que vale la asignatura");
+        System.out.println("2.- Clave de la asignatura");
+        System.out.println("3.- Semestre al que pertenece la asignatura");
+        System.out.println("4.- Creditos que vale la asignatura");
         System.out.println("\topcion: ");
         Integer opcion = scanner.nextInt();
         System.out.println("\n");
         return opcion;
     }
+    
+    /**
+     * menú dirigido al estudiante
+     * @return
+     */
+    public Integer menuCargaAcademica() {
+    	System.out.println("\n\tMenú de Carga academica\n");
+    	System.out.println("1.- Cargar asignatura");
+    	System.out.println("2.- Mostrar asignaturas cargadas");
+    	System.out.println("3.- Eliminar asignatura");
+    	System.out.println("\topcion: ");
+    	Integer opcion = scanner.nextInt();
+    	return opcion;
+    }
+    
     public void imprimeInfoBorrada(boolean e) {
     	if(e) {
     		msgBaja();
     	}else {
-    		msgnoRegistro();
+    		msgNoRegistro();
     	}
     }
+    
+    
     public void msgBaja() {
     	System.out.println("\n\tborrado exitosamente\n");
     }
-    public void msgnoRegistro(){
-    	System.out.println("\n\tno se encuentra la materia\n");
+    public void msgNoRegistro(){
+    	System.out.println("\n\tno se encuentra la asignatura\n");
     }
     public void msgError() {
     	System.out.println("\n\tHa ocurrido un error\n");
     }
     public void msgVuelvaPronto() {
-    	System.out.println("\n\tSaliendo del menu Estudiante...");
+    	System.out.println("\n\tSaliendo del menu Asignatura...");
     }
 }
