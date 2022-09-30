4b406b07118731221e0af7583f4a9990ce935086
Angel Balderas <92223336+a-bals21@users.noreply.github.com>
2022-09-29 21:44:42 -0500

    Update Asignatura.java
    Corregir conflictos

--- ControlEscolarUMT/src/com/bals/Asignatura.java
+++ ControlEscolarUMT/src/com/bals/Asignatura.java
@@ -1,70 +1,80 @@
 package com.bals;
 
 /**
-* Esta clase abstrae los posible datos de una asignatura necesarios para un control de ellos
-* @author Angel Balderas
-* @version 0.0.1
-*/
+ * La clase Asignatura es la abstracción de los atributos que contiene una asignatura
+ * @version 0.1.1, 24/09/22
+ * @author Angel Balderas
+ *
+ */
 public class Asignatura {
+	
+	//ATRIBUTOS
+	private String clave;
+	private String nombre;
+	private Integer semestre;
+	private Integer credito;
+	//posible promedio en asignatura
+	
+	//CONTRUCTORES
+	Asignatura() {
+		setClave("NULL");
+		setNombre("NULL");
+		setSemestre(0);
+		setCredito(0);
+	}
+	
+	Asignatura (
+			String clave,
+			String nombre,
+			Integer semestre,
+			Integer credito
+			) {
+		this.clave = clave;
+		this.nombre = nombre;
+		this.semestre = semestre;
+		this.credito = credito;
+	}
+	
+	//SETTERS AND GETTERS
+	public String getClave() {
+		return clave;
+	}
 
-    //ATRIBUTOS
-    private String clave;
-    private String nombre;
-    private Integer semestre;
-    private Integer credito;
-    //posible promedio en asignatura
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
 
-    //CONTRUCTORES
-    Asignatura() {
-        setClave("NULL");
-        setNombre("NULL");
-        setSemestre(0);
-        setCredito(0);
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    Asignatura(
-            String clave,
-            String nombre,
-            Integer semestre,
-            Integer credito
-    ) {
-        this.clave = clave;
-        this.nombre = nombre;
-        this.semestre = semestre;
-        this.credito = credito;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    //SETTERS AND GETTERS
-    public String getClave() {
-        return clave;
-    }
-    
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-    
-    public String getNombre() {
-        return nombre;
-    }
+	public Integer getSemestre() {
+		return semestre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setSemestre(Integer semestre) {
+		this.semestre = semestre;
+	}
 
-    public Integer getSemestre() {
-        return semestre;
-    }
+	public Integer getCredito() {
+		return credito;
+	}
 
-    public void setSemestre(Integer semestre) {
-        this.semestre = semestre;
-    }
+	public void setCredito(Integer credito) {
+		this.credito = credito;
+	}
 
-    public Integer getCredito() {
-        return credito;
-    }
-
-    public void setCredito(Integer credito) {
-        this.credito = credito;
-    }
-
+	@Override
+	public String toString() {
+		return "Asignatura{" +
+				"clave='" + clave + '\'' +
+				", nombre='" + nombre + '\'' +
+				", semestre=" + semestre +
+				", credito=" + credito +
+				'}';
+	}
 }
