1acdbee71e95d76cc84314f5701a99d587246172
Angel Balderas <92223336+a-bals21@users.noreply.github.com>
2022-09-29 21:46:45 -0500

    Update Main.java
    Corregir conflictos

--- ControlEscolarUMT/src/com/bals/Main.java
+++ ControlEscolarUMT/src/com/bals/Main.java
@@ -1,18 +1,14 @@
 package com.bals;
 
-/**
- * 
- * @author Angel Balderas
- * @version 0.2.1, 24/09/22
- */
 public class Main {
     //MAIN
     public static void main(String[] args) {
     	CatalogoPrincipal vista = new CatalogoPrincipal();
     	CatalogoEstudiante vistaEstudiante = new CatalogoEstudiante();
     	CatalogoProfesor vistaProfesor = new CatalogoProfesor();
+	CatalogoAsignatura vistaAsignatura = new CatalogoAsignatura();
     	
-    	ControladorPrincipal programa = new ControladorPrincipal(vista, vistaEstudiante, vistaProfesor);
+    	ControladorPrincipal programa = new ControladorPrincipal(vista, vistaEstudiante, vistaProfesor, vistaAsignatura);
     	
     	programa.menuPrincipal();
         
