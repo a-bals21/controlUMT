d918535af8ac7c9c182624c5271134cab57445d8
Angel Balderas <92223336+a-bals21@users.noreply.github.com>
2022-09-29 21:52:40 -0500

    Update CatalogoAsignatura.java
    Corregí conflictos

--- ControlEscolarUMT/src/com/bals/CatalogoAsignatura.java
+++ ControlEscolarUMT/src/com/bals/CatalogoAsignatura.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 /**
  * Esta clase lee informacion relacionada con las asignaturas en el semestre.
- * @autor kevin leandro camelo suaste,
+ * @autor Kevin Camelo, Erika Aguilar
  * 
  */
 public class CatalogoAsignatura {
@@ -14,32 +14,33 @@ public class CatalogoAsignatura {
     private Scanner scanner = new Scanner(System.in);
 
     //metodos
-    public String SolicitarAsignatura(){
-        System.out.println("Escribe el nombre de la asignatura: ");
+    public String solicitarNombre(){
+        System.out.print("Escribe el nombre de la asignatura: ");
         return scanner.nextLine();
     }
 
-    public String SolicitarClave() {
-    	System.out.println("Escribe la clave de la materia");
+    public String solicitarClave() {
+        scanner.nextLine();
+    	System.out.print("Escribe la clave de la asignatura: ");
     	return scanner.nextLine();
     }
     
-    public Integer SolicitarSemestre(){
-        System.out.println("Escribe la asignatura a la que pertenece");
+    public Integer solicitarSemestre(){
+        System.out.print("Escribe el semestre al que pertenece: ");
         return scanner.nextInt();
     }
     
-    public Integer SolicitarCreditos(){
-        System.out.println("Escribe los creditos que vale la asignatura");
+    public Integer solicitarCredito(){
+        System.out.print("Escribe los creditos que vale la asignatura: ");
         return scanner.nextInt();
     }
     
-    public void ReadAsignaturas (Asignatura asignatura){
-        System.out.println("\n\tAsignatura");
+    public void readAsignatura(Asignatura asignatura){
+        System.out.print("\n\tAsignatura");
         System.out.println(asignatura);
     }
     
-    public void readAsignatura(){System.out.println("\n\tNo existe materia conocida\n");}
+    public void readAsignatura(){System.out.println("\n\tNo existe esa asignatura\n");}
 
     //metodo
     public void readTAsignatura(ArrayList<Asignatura> asignaturas){
@@ -50,41 +51,41 @@ public class CatalogoAsignatura {
     
     //metodo
     public Integer menu(){
-        System.out.println("\n\tMENÚ ASIGNATURA\n");
+        System.out.print("\n\tMENÚ ASIGNATURA\n");
         System.out.println("1.- agregar asignatura");
         System.out.println("2.- borrar asignatura");
         System.out.println("3.- actualizar la asignatura");
         System.out.println("4.- desplegar lista de asignaturas");
         System.out.println("5.- buscar asignaturas");
         System.out.println("6.- salir");
-        System.out.println("\topcion: ");
+        System.out.print("\topcion: ");
         Integer opcion = scanner.nextInt();
-        System.out.println("\n");
+        System.out.print("\n");
         return opcion;
     }
 
     public Integer menuModificarAsignatura(){
-        System.out.println("\n\tMenú Modificación\n");
+        System.out.print("\n\tMenú Modificación\n");
         System.out.println("1.- Nombre de la asignatura");
         System.out.println("2.- Clave de la asignatura");
         System.out.println("3.- Semestre al que pertenece la asignatura");
         System.out.println("4.- Creditos que vale la asignatura");
-        System.out.println("\topcion: ");
+        System.out.print("\topcion: ");
         Integer opcion = scanner.nextInt();
-        System.out.println("\n");
+        System.out.print("\n");
         return opcion;
     }
     
     /**
      * menú dirigido al estudiante
-     * @return
+     * @return opcion
      */
     public Integer menuCargaAcademica() {
     	System.out.println("\n\tMenú de Carga academica\n");
     	System.out.println("1.- Cargar asignatura");
     	System.out.println("2.- Mostrar asignaturas cargadas");
     	System.out.println("3.- Eliminar asignatura");
-    	System.out.println("\topcion: ");
+    	System.out.print("\topcion: ");
     	Integer opcion = scanner.nextInt();
     	return opcion;
     }
@@ -96,7 +97,10 @@ public class CatalogoAsignatura {
     		msgNoRegistro();
     	}
     }
-    
+
+    public void msgActualizado() {
+        System.out.print("\n\tActualización EXITOSA\n");
+    }
     
     public void msgBaja() {
     	System.out.println("\n\tborrado exitosamente\n");
