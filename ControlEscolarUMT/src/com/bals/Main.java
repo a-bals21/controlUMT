32cd66f71c67b2b56ec864e3ecc2d410f60e28b1
yami9597 <92878301+yami9597@users.noreply.github.com>
2022-09-29 21:37:53 -0500

    Update Main.java

--- ControlEscolarUMT/src/com/bals/Main.java
+++ ControlEscolarUMT/src/com/bals/Main.java
@@ -6,7 +6,7 @@ public class Main {
     	CatalogoPrincipal vista = new CatalogoPrincipal();
     	CatalogoEstudiante vistaEstudiante = new CatalogoEstudiante();
     	CatalogoProfesor vistaProfesor = new CatalogoProfesor();
-		CatalogoAsignatura vistaAsignatura = new CatalogoAsignatura();
+	CatalogoAsignatura vistaAsignatura = new CatalogoAsignatura();
     	
     	ControladorPrincipal programa = new ControladorPrincipal(vista, vistaEstudiante, vistaProfesor, vistaAsignatura);
     	
